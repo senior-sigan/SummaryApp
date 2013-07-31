178d6ce88d9a8a8188cd7cdbfafec507010e4e6a
Ilya Siganov <ilya.blan4@gmail.com>
2013-07-31 22:53:55 +0700

    new test for ParticipationImport and include activeModel::Model

--- app/models/participation_import.rb
+++ app/models/participation_import.rb
@@ -1,3 +1,14 @@
 class ParticipationImport
+	include ActiveModel::Model
 	attr_accessor :file
+
+	def save
+	  #load imported data
+	  #validate each Participation row 
+	  #on invalid push error and return false
+	  #else return true
+	end
+
+	def open_spreadsheet
+	end
 end
--- spec/models/participation_import_spec.rb
+++ spec/models/participation_import_spec.rb
@@ -1,6 +1,32 @@
 require 'spec_helper'
 
 describe ParticipationImport do
+  before { @import = ParticipationImport.new }
+
+  subject { @import }
+
   it { should respond_to(:file) }
   it { should respond_to(:save) }
+  it { should respond_to(:errors) }
+
+  it { should be_valid }
+
+  describe "when Participation in file is invalid" do
+  	before { @import.file = "Some crazy stuff" } # TODO - real csv with crazy rows
+  	it { should_not be_valid }
+  end
+
+  describe "when Participation in file is valid" do
+  	before { @import.file = "Some true stuff" } # TODO - real csv with true rows
+  	it { should be_valid }
+  end
+
+  describe "when User not exists it creates new one" do
+  	#TODO
+  end
+
+  describe "when User exists it updates this one" do
+  	#TODO
+  end 
+
 end

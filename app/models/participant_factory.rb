588b9bccd470ef275cf55cd17ba6ebca14b1bff2
blan4 <ilya.blan4@gmail.com>
2014-03-08 15:02:59 +0700

    add func to finding email in existing participants by name and surname

--- app/models/participant_factory.rb
+++ app/models/participant_factory.rb
@@ -8,7 +8,7 @@ class ParticipantFactory
   end
 
   def self.construct(event, email, name, surname)
-    email ||= generate_email(name, surname)
+    email ||=  CalculatedParticipant.where(:'value.name' => name, :'value.surname' => surname).distinct(:id).last || generate_email(name, surname)
     return nil if email.blank?
 
     participant = event.participants.find_or_initialize_by(email: email.mb_chars.downcase.to_s)

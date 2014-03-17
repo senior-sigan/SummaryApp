087ac5fb72f23cba35872d697ce340b536fb29af
blan4 <ilya.blan4@gmail.com>
2014-03-17 11:01:40 +0700

    remove seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-owner = Owner.new(email: 'ilya.blan4@gmail.com', approved: true)
-owner.password = 'qwerty'
-owner.password_confirmation = 'qwerty'
-owner.save

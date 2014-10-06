37d37b85775d3bfa8a87025da78c692e7613d214
Ilya Siganov <ilya.blan4@gmail.com>
2014-10-06 22:49:03 +0700

    make participants list simpler

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 # TODO it's just test
-@ev1 = Event.create(name: 'event_2', date: DateTime.now - 1.day)
-@ev2 = Event.create(name: 'event_1', date: DateTime.now - 2.day)
-@ev3 = Event.create(name: 'event_3', date: DateTime.now)
+ev1 = Event.create(name: 'event_2', date: DateTime.now - 1.day)
+ev2 = Event.create(name: 'event_1', date: DateTime.now - 2.day)
+ev3 = Event.create(name: 'event_3', date: DateTime.now)
 
-RecordsImporter.new(@ev1, File.new("#{Rails.root}/spec/files/event_2.csv")).save
-RecordsImporter.new(@ev2, File.new("#{Rails.root}/spec/files/event_1.csv")).save
-RecordsImporter.new(@ev3, File.new("#{Rails.root}/spec/files/event_3.csv")).save
+r1 = RecordsImporter.new(ev1)
+r2 = RecordsImporter.new(ev2)
+r3 = RecordsImporter.new(ev3)
+
+r1.file = File.new("#{Rails.root}/spec/files/event_2.csv")
+r2.file = File.new("#{Rails.root}/spec/files/event_1.csv")
+r3.file = File.new("#{Rails.root}/spec/files/event_3.csv")
+
+[r1,r2,r3].each(&:save)
